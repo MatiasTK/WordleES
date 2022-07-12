8b2e395df95680aff714a0b4f1ae2ec3b2c3aa43
Matias Vallejos <matiavallejo@gmail.com>
2022-07-12 09:35:57 -0300

    removed unused things

--- src/libs/crypto.js
+++ src/libs/crypto.js
@@ -3,7 +3,6 @@ import CryptoJS from 'crypto-js';
 const secretKey = process.env.REACT_APP_SECRET_KEY;
 
 function encriptarPalabra(palabra) {
-  console.log(palabra);
   return CryptoJS.AES.encrypt(palabra, secretKey).toString();
 }
 
